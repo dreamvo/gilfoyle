e916a27b730cc9c7ae02fe60f930c2b32360be6d
sundowndev <raphael@crvx.fr>
2020-11-07 12:45:41 +0100

    feat: create version command

---
+++ cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/dreamvo/gilfoyle/config"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	// Register command
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print program version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Gilyfoyle version %s commit %s\n", config.Version, config.Commit)
+	},
+}
