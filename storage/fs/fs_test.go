7494460ca85888ca54661004d34a7340d6b9d654
sundowndev <raphael@crvx.fr>
2020-11-15 18:05:07 +0100

    refactor(storage): put interface in the internal pkg

---
+++ storage/fs/fs_test.go
@@ -0,0 +1,44 @@
+package fs
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"github.com/dreamvo/gilfoyle/storage"
+	"io/ioutil"
+	"testing"
+	"time"
+)
+
+func Test(t *testing.T) {
+	dir, err := ioutil.TempDir("", "storage-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	s := NewStorage(Config{Root: dir})
+	ctx := context.Background()
+
+	if _, err = s.Stat(ctx, "doesnotexist"); !errors.Is(err, storage.ErrNotExist) {
+		t.Errorf("expected not exists, got %v", err)
+	}
+
+	before := time.Now()
+	if err := s.Save(ctx, bytes.NewBufferString("hello"), "world"); err != nil {
+		t.Fatal(err)
+	}
+	now := time.Now()
+
+	stat, err := s.Stat(ctx, "world")
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+	if stat.Size != 5 {
+		t.Errorf("expected size to be %d, got %d", 5, stat.Size)
+	}
+	if stat.ModifiedTime.Before(before) {
+		t.Errorf("expected modtime to be after %v, got %v", before, stat.ModifiedTime)
+	}
+	if stat.ModifiedTime.After(now) {
+		t.Errorf("expected modtime to be before %v, got %v", now, stat.ModifiedTime)
+	}
+}
--- storage/storage.go
+++ storage/storage.go
@@ -1,1 +1,25 @@
 package storage
+
+import (
+	"context"
+	"errors"
+	"io"
+	"time"
+)
+
+// Storage is the storage interface.
+type Storage interface {
+	Save(ctx context.Context, content io.Reader, path string) error
+	Stat(ctx context.Context, path string) (*Stat, error)
+	Open(ctx context.Context, path string) (io.ReadCloser, error)
+	Delete(ctx context.Context, path string) error
+}
+
+// Stat contains metadata about content stored in storage.
+type Stat struct {
+	ModifiedTime time.Time
+	Size         int64
+}
+
+// ErrNotExist is a sentinel error returned by the Open and the Stat methods.
+var ErrNotExist = errors.New("does not exist")
