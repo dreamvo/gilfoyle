0856354f4b0234969be5e49cee1d8fe7e15f0492
sundowndev <raphael@crvx.fr>
2020-12-08 20:27:52 +0400

    test: transcoding utils

--- transcoding/util_test.go
+++ transcoding/util_test.go
@@ -4,15 +4,16 @@ import "testing"
 
 func TestUtil(t *testing.T) {
 	var suite = map[string]int8{
-		"25/1": 25,
-		"30/1": 30,
-		"16/1": 16,
-		"30":   30,
-		"25":   25,
+		"25/1":    25,
+		"30/1":    30,
+		"16/1":    16,
+		"30":      30,
+		"25":      25,
+		"invalid": 0,
 	}
 
 	for input, expected := range suite {
-		if fps := ParseFrameRates(input); fps == 0 {
+		if fps := ParseFrameRates(input); fps != expected {
 			t.Errorf("expected: %d, got: %d", expected, fps)
 		}
 	}
