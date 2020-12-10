3674c5fd318f1b2517556327d81ac6ceecd3c64c
sundowndev <raphael@crvx.fr>
2020-12-10 17:18:45 +0400

    chore: fix lint errors

--- worker/mocks/logger.go
+++ worker/mocks/logger.go
@@ -11,15 +11,12 @@ type MockedLogger struct {
 
 func (m *MockedLogger) Error(msg string, fields ...zap.Field) {
 	m.Called(msg, fields)
-	return
 }
 
 func (m *MockedLogger) Info(msg string, fields ...zap.Field) {
 	m.Called(msg, fields)
-	return
 }
 
 func (m *MockedLogger) Fatal(msg string, fields ...zap.Field) {
 	m.Called(msg, fields)
-	return
 }
