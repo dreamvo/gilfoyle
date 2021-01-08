f1686248db92db0227fa80741f1a9ab3ea7ee216
Remi <remi.d45@gmail.com>
2021-01-08 13:18:29 +0100

    feat(storage): s3 tests - dockertest
    fix(storage): remove logs
    
    feat(storage): s3 tests with dockertest
    
    feat(storage): remove gcs tests

--- storage/s3/s3_test.go
+++ storage/s3/s3_test.go
@@ -3,25 +3,73 @@ package s3_test
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"testing"
+	"time"
+
 	"github.com/dreamvo/gilfoyle"
 	"github.com/dreamvo/gilfoyle/config"
 	"github.com/dreamvo/gilfoyle/storage"
-	"github.com/google/uuid"
+	"github.com/ory/dockertest"
+	"github.com/ory/dockertest/docker"
 	assertTest "github.com/stretchr/testify/assert"
-	"io/ioutil"
-	"testing"
-	"time"
 )
 
 func TestS3(t *testing.T) {
 	assert := assertTest.New(t)
+	var err error
+
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	options := &dockertest.RunOptions{
+		Repository: "minio/minio",
+		Tag:        "latest",
+		Cmd:        []string{"server", "/data"},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"9000/tcp": []docker.PortBinding{{HostPort: "9000"}},
+		},
+		Env: []string{"MINIO_ACCESS_KEY=access_key", "MINIO_SECRET_KEY=secret_key"},
+	}
+
+	resource, err := pool.RunWithOptions(options, func(config *docker.HostConfig) {
+		// set AutoRemove to true so that stopped container goes away by itself
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{
+			Name: "no",
+		}
+	})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	endpoint := fmt.Sprintf("localhost:%s", resource.GetPort("9000/tcp"))
+
+	if err := pool.Retry(func() error {
+		url := fmt.Sprintf("http://%s/minio/health/live", endpoint)
+		resp, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("status code not OK")
+		}
+		return nil
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
 
 	gilfoyle.Config.Storage.S3 = config.S3Config{
-		Hostname:        "play.min.io",
-		AccessKeyID:     "Q3AM3UQ867SPQQA43P2F",
-		SecretAccessKey: "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
-		Bucket:          uuid.New().String(),
-		EnableSSL:       true,
+		Hostname:        endpoint,
+		AccessKeyID:     "access_key",
+		SecretAccessKey: "secret_key",
+		Bucket:          "gilfoyle-aws-bucket",
+		EnableSSL:       false,
 	}
 
 	t.Run("should return error file does not exist", func(t *testing.T) {
@@ -98,4 +146,17 @@ func TestS3(t *testing.T) {
 		assert.NoError(err)
 		assert.Equal("hello", string(b))
 	})
+
+	t.Run("should delete the file", func(t *testing.T) {
+		s, err := gilfoyle.NewStorage(storage.AmazonS3)
+		assert.NoError(err)
+
+		ctx := context.Background()
+
+		err = s.Delete(ctx, "world")
+		assert.NoError(err)
+	})
+	if err = pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
 }
