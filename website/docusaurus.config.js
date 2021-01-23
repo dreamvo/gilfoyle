97ca5ef674a13cd606be12513e135c6aa034b542
sundowndev <raphael@crvx.fr>
2021-01-23 17:15:06 +0400

    refactor(docs): update style

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,15 +11,27 @@ module.exports = {
     themeConfig: {
         navbar: {
             title: 'Gilfoyle',
-            logo: {
-                alt: 'Gilfoyle logo',
-                src: 'img/logo.svg',
-            },
+            // logo: {
+            //     alt: 'Gilfoyle logo',
+            //     src: 'img/logo.svg',
+            // },
             items: [
                 {
                     to: 'docs/',
                     activeBasePath: 'docs',
-                    label: 'Documentation',
+                    label: 'Get started',
+                    position: 'left',
+                },
+                {
+                    to: 'docs/',
+                    activeBasePath: 'docs',
+                    label: 'Use cases',
+                    position: 'left',
+                },
+                {
+                    to: 'docs/',
+                    activeBasePath: 'docs',
+                    label: 'Concepts',
                     position: 'left',
                 },
                 {
@@ -28,7 +40,21 @@ module.exports = {
                     label: 'Migration guides',
                     position: 'left',
                 },
-                {to: 'blog', label: 'Blog', position: 'right'},
+                {
+                    to: 'docs/',
+                    activeBasePath: 'docs',
+                    label: 'References',
+                    position: 'left',
+                },
+                {
+                    to: 'https://github.com/dreamvo/gilfoyle/releases',
+                    activeBasePath: 'docs',
+                    label: 'Releases',
+                    position: 'right',
+                },
+                {
+                    to: 'blog', label: 'Blog', position: 'right'
+                },
                 {
                     href: 'https://github.com/dreamvo/gilfoyle',
                     label: 'GitHub',
@@ -40,15 +66,23 @@ module.exports = {
             style: 'dark',
             links: [
                 {
-                    title: 'Docs',
+                    title: 'Learn',
                     items: [
                         {
-                            label: 'Style Guide',
+                            label: 'Getting started',
                             to: 'docs/',
                         },
                         {
-                            label: 'Second Doc',
+                            label: 'Deploy on Kubernetes',
                             to: 'docs/doc2/',
+                        },
+                        {
+                            label: 'Manage a worker pool',
+                            to: 'docs/doc2/',
+                        },
+                        {
+                            label: 'OpenAPI specs',
+                            to: 'https://petstore.swagger.io/?url=https://raw.githubusercontent.com/dreamvo/gilfoyle/master/api/docs/swagger.json',
                         },
                     ],
                 },
@@ -56,15 +90,15 @@ module.exports = {
                     title: 'Community',
                     items: [
                         {
-                            label: 'Stack Overflow',
+                            label: 'Discuss on GitHub',
                             href: 'https://stackoverflow.com/questions/tagged/docusaurus',
                         },
                         {
-                            label: 'Discord',
+                            label: 'Report a bug',
                             href: 'https://discordapp.com/invite/docusaurus',
                         },
                         {
-                            label: 'Twitter',
+                            label: 'Follow us on Twitter',
                             href: 'https://twitter.com/dreamvoapp',
                         },
                     ],
