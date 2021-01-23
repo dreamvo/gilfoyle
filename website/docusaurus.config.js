791861b3272d801f0c3a0006d41728f8a2cb60b8
sundowndev <raphael@crvx.fr>
2021-01-23 16:26:32 +0400

    refactor(docs): move docs to website folder

---
+++ website/docusaurus.config.js
@@ -0,0 +1,111 @@
+module.exports = {
+    title: 'Gilfoyle',
+    tagline: 'Cloud-native solution to embed media streaming in any application at any scale.',
+    url: 'https://docs.gilfoyle.dreamvo.com',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'dreamvo', // Usually your GitHub org/user name.
+    projectName: 'gilfoyle', // Usually your repo name.
+    themeConfig: {
+        navbar: {
+            title: 'Gilfoyle',
+            logo: {
+                alt: 'Gilfoyle logo',
+                src: 'img/logo.svg',
+            },
+            items: [
+                {
+                    to: 'docs/',
+                    activeBasePath: 'docs',
+                    label: 'Documentation',
+                    position: 'left',
+                },
+                {
+                    to: 'docs/',
+                    activeBasePath: 'docs',
+                    label: 'Migration guides',
+                    position: 'left',
+                },
+                {to: 'blog', label: 'Blog', position: 'right'},
+                {
+                    href: 'https://github.com/dreamvo/gilfoyle',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Style Guide',
+                            to: 'docs/',
+                        },
+                        {
+                            label: 'Second Doc',
+                            to: 'docs/doc2/',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Stack Overflow',
+                            href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                        },
+                        {
+                            label: 'Discord',
+                            href: 'https://discordapp.com/invite/docusaurus',
+                        },
+                        {
+                            label: 'Twitter',
+                            href: 'https://twitter.com/dreamvoapp',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'Blog',
+                            to: 'blog',
+                        },
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/dreamvo/gilfoyle',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Dreamvo, Inc.`,
+        },
+    },
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            {
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    editUrl:
+                        'https://github.com/dreamvo/gilfoyle/edit/master/website/',
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    editUrl:
+                        'https://github.com/dreamvo/gilfoyle/edit/master/website/blog/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            },
+        ],
+    ],
+};
