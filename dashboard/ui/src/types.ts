b6c546bad68e7dc69909e210e20d8920075aec84
sundowndev <raphael@crvx.fr>
2021-03-05 23:00:25 +0100

    refactor(dashboard): UI improvements

--- dashboard/ui/src/types.ts
+++ dashboard/ui/src/types.ts
@@ -1,40 +1,45 @@
 export interface MediaFile {
-  id: string;
-  media: string;
-  rendition_name: string;
-  resolution_width: number;
-  resolution_height: number;
-  target_bandwidth: number;
-  framerate: number;
-  duration_seconds: number;
-  media_type: string;
-  created_at: string;
-  updated_at: string;
+    id: string;
+    media: string;
+    status: string
+    message: string
+    rendition_name: string;
+    entry_file: string
+    mimetype: string
+    resolution_width: number;
+    resolution_height: number;
+    target_bandwidth: number;
+    framerate: number;
+    duration_seconds: number;
+    media_type: string;
+    created_at: string;
+    updated_at: string;
 }
 
 export interface Media {
-  id: string;
-  title: string;
-  status: string;
-  created_at: string;
-  updated_at: string;
-  edges: {
-    media_files?: MediaFile[];
-  };
+    id: string;
+    title: string;
+    status: string;
+    message: string
+    created_at: string;
+    updated_at: string;
+    edges: {
+        media_files?: MediaFile[];
+    };
 }
 
 export interface ArrayResponse<T = unknown> {
-  code: number;
-  metadata: { [key: string]: unknown };
-  data: T[];
+    code: number;
+    metadata: { [key: string]: unknown };
+    data: T[];
 }
 
 export interface DataResponse<T = unknown> {
-  code: number;
-  data: T;
+    code: number;
+    data: T;
 }
 
 export interface Source {
-  src: string;
-  type: string;
+    src: string;
+    type: string;
 }
--- dashboard/ui/src/views/MediaView.vue
+++ dashboard/ui/src/views/MediaView.vue
@@ -2,28 +2,29 @@
   <v-row>
     <v-col md="6" sm="12">
       <v-card
-        flat
-        outlined
-        :loading="loading || processing"
-        :disabled="loading"
+          flat
+          outlined
+          :loading="loading || isProcessing"
+          :disabled="loading"
       >
         <v-card-title
-          >{{ media.title }}
-          <v-chip :color="statusLevel" small class="ml-2"
-            >{{ media.status }}
+        >{{ media.title }}
+          <v-chip :color="statusLevel(media.status)" small class="ml-2"
+          >{{ media.status }}
           </v-chip>
         </v-card-title>
         <v-card-subtitle
-          >Created on {{ media.created_at | readableDateHour }}</v-card-subtitle
+        >Created on {{ media.created_at | readableDateHour }}
+        </v-card-subtitle
         >
 
         <v-card-subtitle class="mt-1">
           <v-text-field
-            label="Identifier"
-            outlined
-            dense
-            readonly
-            :value="media.id"
+              label="Identifier"
+              outlined
+              dense
+              readonly
+              :value="media.id"
           ></v-text-field>
         </v-card-subtitle>
 
@@ -31,12 +32,12 @@
           <v-dialog v-model="uploadDialog" width="500">
             <template v-slot:activator="{ on, attrs }">
               <v-btn
-                v-if="awaitingUpload"
-                text
-                color="primary"
-                small
-                v-bind="attrs"
-                v-on="on"
+                  v-if="awaitingUpload"
+                  text
+                  color="primary"
+                  small
+                  v-bind="attrs"
+                  v-on="on"
               >
                 <v-icon size="16" dark class="mr-1">
                   mdi-upload
@@ -47,19 +48,19 @@
 
             <v-card>
               <MediaUploadForm
-                title="Upload a media file"
-                :media-id="media.id"
+                  title="Upload a media file"
+                  :media-id="media.id"
               />
             </v-card>
           </v-dialog>
 
           <v-btn
-            small
-            text
-            color="secondary"
-            dark
-            class="ml-2"
-            :to="{ name: 'MediaUpdate', id: media.id }"
+              small
+              text
+              color="secondary"
+              dark
+              class="ml-2"
+              :to="{ name: 'MediaUpdate', id: media.id }"
           >
             <v-icon size="16" dark class="mr-1">
               mdi-pencil
@@ -67,65 +68,65 @@
             Update
           </v-btn>
 
-          <DeleteModal btn-small btn-text="Delete" :action="deleteMedia" />
+          <DeleteModal btn-small btn-text="Delete" :action="deleteMedia"/>
         </v-card-actions>
       </v-card>
 
       <v-card
-        flat
-        outlined
-        :loading="loading"
-        :disabled="loading"
-        class="mt-6"
-        v-if="media.edges && media.edges.media_files"
+          flat
+          outlined
+          :loading="loading"
+          :disabled="loading"
+          class="mt-6"
+          v-if="media.edges && media.edges.media_files"
       >
         <v-card-title>Renditions</v-card-title>
 
         <v-simple-table>
           <template v-slot:default>
             <thead>
-              <tr>
-                <th class="text-left">
-                  Status
-                </th>
-                <th class="text-left">
-                  Name
-                </th>
-                <th class="text-left">
-                  Format
-                </th>
-                <th class="text-left">
-                  Resolution
-                </th>
-                <th class="text-left">
-                  Target bandwidth
-                </th>
-                <th class="text-left">
-                  Framerate
-                </th>
-                <th class="text-left">
-                  Duration
-                </th>
-                <th class="text-left">
-                  Media type
-                </th>
-              </tr>
+            <tr>
+              <th class="text-left">
+                Status
+              </th>
+              <th class="text-left">
+                Name
+              </th>
+              <th class="text-left">
+                Format
+              </th>
+              <th class="text-left">
+                Resolution
+              </th>
+              <th class="text-left">
+                Target bandwidth
+              </th>
+              <th class="text-left">
+                Framerate
+              </th>
+              <th class="text-left">
+                Duration
+              </th>
+              <th class="text-left">
+                Media type
+              </th>
+            </tr>
             </thead>
             <tbody>
-              <tr v-for="item in media.edges.media_files" :key="item.id">
-                <td>
-                  <v-chip color="success" small>Ready</v-chip>
-                </td>
-                <td>{{ item.rendition_name }}</td>
-                <td>{{ item.format }}</td>
-                <td>
-                  {{ item.resolution_width }}x{{ item.resolution_height }}
-                </td>
-                <td>{{ item.target_bandwidth }}</td>
-                <td>{{ item.framerate }} FPS</td>
-                <td>{{ Math.round(item.duration_seconds) }} sec</td>
-                <td>{{ item.media_type }}</td>
-              </tr>
+            <tr v-for="item in media.edges.media_files" :key="item.id">
+              <td>
+                <v-chip :color="statusLevel(item.status)" small>{{ item.status }}</v-chip>
+              </td>
+              <td>{{ item.rendition_name }}</td>
+              <td>{{ item.format }}</td>
+              <td>
+                {{ item.resolution_width }}x{{ item.resolution_height }}
+              </td>
+              <td>{{ item.target_bandwidth }}</td>
+              <td>{{ item.framerate }} FPS</td>
+              <td>{{ Math.round(item.duration_seconds) }} sec</td>
+              <td>{{ item.media_type }}</td>
+            </tr>
             </tbody>
           </template>
         </v-simple-table>
@@ -137,11 +138,11 @@
         <v-card-title>Streaming</v-card-title>
 
         <v-card-text
-          ><span v-if="!streamReady"
-            >This media is not yet available for streaming.</span
-          >
+        ><span v-if="!isStreamReady"
+        >This media is not yet available for streaming.</span
+        >
           <div v-else class="">
-            <VideoPlayer :sources="sources" />
+            <VideoPlayer :sources="sources"/>
           </div>
         </v-card-text>
       </v-card>
@@ -150,9 +151,9 @@
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-property-decorator";
-import { AxiosResponse } from "axios";
-import { DataResponse, Media, MediaFile, Source } from "../types";
+import {Vue} from "vue-property-decorator";
+import {AxiosResponse} from "axios";
+import {DataResponse, Media, MediaFile, Source} from "../types";
 import axios from "../services/axios";
 import DeleteModal from "../components/DeleteModal.vue";
 import MediaUploadForm from "../components/MediaUploadForm.vue";
@@ -160,17 +161,15 @@ import VideoPlayer from "../components/VideoPlayer.vue";
 
 interface Data {
   loading: boolean;
-  streamReady: boolean;
   uploadDialog: boolean;
   media: Media;
   sources: Source[];
 }
 
 export default Vue.extend({
-  components: { VideoPlayer, MediaUploadForm, DeleteModal },
+  components: {VideoPlayer, MediaUploadForm, DeleteModal},
   data: (): Data => ({
     loading: true,
-    streamReady: false,
     uploadDialog: false,
     media: {} as Media,
     sources: []
@@ -179,13 +178,13 @@ export default Vue.extend({
     async deleteMedia() {
       await axios.delete(`/medias/${this.$route.params.id}`);
 
-      await this.$router.push({ name: "MediaAll" });
+      await this.$router.push({name: "MediaAll"});
     },
     rendition(rendition: MediaFile) {
       this.sources = [
         {
-          src: `${axios.defaults.baseURL}/medias/${this.media.id}/stream/${rendition.rendition_name}/index.m3u8`,
-          type: "application/x-mpegURL"
+          src: `${axios.defaults.baseURL}/medias/${this.media.id}/stream/${rendition.rendition_name}/${rendition.entry_file}`,
+          type: rendition.mimetype,
         }
       ];
     },
@@ -196,37 +195,55 @@ export default Vue.extend({
           type: "application/x-mpegURL"
         }
       ];
-    }
-  },
-  computed: {
-    statusLevel(): string {
-      const status: { [key: string]: string } = {
+    },
+    async fetchMedia() {
+      const res: AxiosResponse<DataResponse<Media>> = await axios.get(
+          `/medias/${this.$route.params.id}`
+      );
+
+      this.media = res.data.data;
+
+      if (!this.isProcessing) {
+        clearInterval()
+      }
+    },
+    statusLevel(status: string): string {
+      const statusValues: { [key: string]: string } = {
         Ready: "success",
         Errored: "error",
         AwaitingUpload: "warning"
       };
 
-      return status[this.media.status] || "";
+      return statusValues[status] || "default";
     },
+  },
+  computed: {
     isReady(): boolean {
       return this.media.status === "Ready";
     },
-    processing(): boolean {
+    isProcessing(): boolean {
       return this.media.status === "Processing";
     },
     awaitingUpload(): boolean {
       return this.media.status === "AwaitingUpload";
+    },
+    isStreamReady(): boolean {
+      if (this.media.edges.media_files && this.media.edges.media_files.length == 0) {
+        return false
+      }
+
+      return this.media.status === 'Ready'
     }
   },
   async created() {
-    const res: AxiosResponse<DataResponse<Media>> = await axios.get(
-      `/medias/${this.$route.params.id}`
-    );
+    await this.fetchMedia()
 
-    this.media = res.data.data;
-    this.streamReady = this.isReady && Object.keys(this.media.edges).length > 0;
 
     this.resetRenditions();
+
+    if (this.isProcessing) {
+      setInterval(this.fetchMedia, 3000)
+    }
 
     this.loading = false;
   }
